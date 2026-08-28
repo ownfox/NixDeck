b0bf197e4f85c0387b1fb7a96b7105825251a919
skubi.dubi.du2111 <skubi.dubi.du2111@gmail.com>
2026-08-28 17:18:14 +0200

    fix: Replace SGDBoop with Steam ROM Manager

--- modules/packages/default.nix
+++ modules/packages/default.nix
@@ -64,7 +64,7 @@
     kdePackages.kcalc         # Калькулятор
     webapp-manager            # Менеджер веб-приложений (как в Bazzite/Mint)
     gearlever                 # Менеджер AppImage файлов (добавляет ярлыки в меню)
-    sgdboop                   # SteamGridDB (менеджер обложек для Steam)
+    steam-rom-manager         # Менеджер ROM-ов для добавления эмуляторов и игр в Steam
     telegram-desktop          # Мессенджер Telegram
   ];
 
